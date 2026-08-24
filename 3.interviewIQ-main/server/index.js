3211e94ff2a96c0c9b6132cd11b4a5c02b047152
Ritesh Tiwari <147159936+riteshtiwarigithub@users.noreply.github.com>
2026-08-24 20:15:35 +0530

    Update index.js
    Added Frontend deployed live link

--- 3.interviewIQ-main/server/index.js
+++ 3.interviewIQ-main/server/index.js
@@ -16,7 +16,7 @@ import paymentRouter from "./routes/payment.route.js";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://interview-iq-1-9f82.onrender.com",
     credentials: true,
   }),
 );
